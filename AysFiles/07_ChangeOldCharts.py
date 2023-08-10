acab2cc5065bbe95ccf459bc7812f4d75decdef4
Ayşenur Durak <64005350+aysnrdurak@users.noreply.github.com>
2023-08-10 11:24:19 +0300

    Update 07_ChangeOldCharts.py
    In this code I'm trying to select specific chart actually it is not working now. But idea is using title as an ID

--- AysFiles/07_ChangeOldCharts.py
+++ AysFiles/07_ChangeOldCharts.py
@@ -1,36 +1,37 @@
 from pptx import Presentation
-from pptx.chart.data import ChartData
 import openpyxl
 
+# PowerPoint dosyasını yükle
 presentation = Presentation('Deneme.pptx')
-wb_chart_details = openpyxl.Workbook()
-ws_chart_details = wb_chart_details.active
-ws_chart_details.title = "Chart Details"
-ws_chart_details.append(["Slide ID", "Chart Type", "Chart Title"])
 
-wb_chart_change = openpyxl.load_workbook("deneme.xlsx")
-sheet = wb_chart_change['Tabelle1']
+# Excel dosyasından yeni verileri yükle
+wb = openpyxl.load_workbook('Deneme.xlsx')
+sheet = wb.active
 new_categories = [cell.value for cell in sheet['A'][1:]]
 new_values = [cell.value for cell in sheet['B'][1:]]
-new_values2 = [cell.value for cell in sheet['C'][1:]]
 
+# Hedef grafik başlığı
+target_chart_title = "Second"
+
+# Her slayttaki şekilleri kontrol et
 for slide in presentation.slides:
     for shape in slide.shapes:
-        print(shape.shape_id)
         if shape.has_chart:
             chart = shape.chart
-
-            #  Detect Charts
-            chart_type = chart.chart_type
             chart_title = chart.chart_title.text_frame.text
-            ws_chart_details.append([slide.slide_id, chart_type, chart_title])
+            
+            # Belirli bir başlığa sahip grafikse
+            if chart_title == target_chart_title:
+                chart_data = chart.chart_data
+                chart_data.categories = new_categories
+                
+                # İlk seriyi al (diğer serileri istediğiniz gibi değiştirebilirsiniz)
+                series = chart_data.series[0]
+                series.values = new_values
+                
+                # Güncellenmiş verilerle grafik verilerini değiştir
+                chart.replace_data(chart_data)
+                print(f"Updated data for the chart with title: {target_chart_title} on slide {slide.slide_id}")
 
-            # Change Charts
-            chart_data = ChartData()
-            chart_data.categories = new_categories
-            chart_data.add_series('Updated Data', new_values)
-            chart_data.add_series('Updated Data', new_values2)
-            chart.replace_data(chart_data)
-
-wb_chart_details.save('deneme_chart_details.xlsx')
-presentation.save('07_ChangeOldCharts.pptx')
+# Güncellenmiş PowerPoint dosyasını kaydet
+presentation.save('updated_presentation.pptx')
