c8577123d6ceface0bb7fa625f5faba05c14410f
Ayşenur Durak <64005350+aysnrdurak@users.noreply.github.com>
2023-08-10 09:49:44 +0300

    Update 07_ChangeOldCharts.py
    In this code I created 3 line in graph but I can not selected specific graph. It changes every graph with same data

--- AysFiles/07_ChangeOldCharts.py
+++ AysFiles/07_ChangeOldCharts.py
@@ -1,36 +1,36 @@
-# For this code we need an excel file. This excel file will include our chart datas. This code now draw just one line.
-
-import openpyxl
-
-# Load the new Excel file
-wb = openpyxl.load_workbook('ornek.xlsx')
-
-# Access the sheet containing the new data
-sheet = wb['Tabelle1']
-
-# Read the new categories and values from the Excel file
-new_categories = [cell.value for cell in sheet['A'][1:]]
-new_values = [cell.value for cell in sheet['B'][1:]]
-
 from pptx import Presentation
 from pptx.chart.data import ChartData
+import openpyxl
 
-# Load the PowerPoint file
-presentation = Presentation('ornek.pptx')
+presentation = Presentation('Deneme.pptx')
+wb_chart_details = openpyxl.Workbook()
+ws_chart_details = wb_chart_details.active
+ws_chart_details.title = "Chart Details"
+ws_chart_details.append(["Slide ID", "Chart Type", "Chart Title"])
 
-# Loop through each slide
+wb_chart_change = openpyxl.load_workbook("deneme.xlsx")
+sheet = wb_chart_change['Tabelle1']
+new_categories = [cell.value for cell in sheet['A'][1:]]
+new_values = [cell.value for cell in sheet['B'][1:]]
+new_values2 = [cell.value for cell in sheet['C'][1:]]
+
 for slide in presentation.slides:
     for shape in slide.shapes:
-        # Check if the shape is a chart
+        print(shape.shape_id)
         if shape.has_chart:
             chart = shape.chart
 
-            # Create ChartData object with new categories and values
+            #  Detect Charts
+            chart_type = chart.chart_type
+            chart_title = chart.chart_title.text_frame.text
+            ws_chart_details.append([slide.slide_id, chart_type, chart_title])
+
+            # Change Charts
             chart_data = ChartData()
             chart_data.categories = new_categories
             chart_data.add_series('Updated Data', new_values)
-
-            # Replace the chart data with the new data
+            chart_data.add_series('Updated Data', new_values2)
             chart.replace_data(chart_data)
 
+wb_chart_details.save('deneme_chart_details.xlsx')
 presentation.save('07_ChangeOldCharts.pptx')
