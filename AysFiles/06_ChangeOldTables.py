2d8b108f277ed2f7cab8c64682c3d2bb05b05257
Ayşenur Durak <64005350+aysnrdurak@users.noreply.github.com>
2023-08-11 11:57:15 +0300

    Update 06_ChangeOldTables.py
    It changes value from specific table

--- AysFiles/06_ChangeOldTables.py
+++ AysFiles/06_ChangeOldTables.py
@@ -1,19 +1,27 @@
 from pptx import Presentation
 
-presentation = Presentation('deneme.pptx')
+# PowerPoint dosyasını yükle
+presentation = Presentation('Cam_details_template_(Draft_V01).pptx')
 
+# Tablo sayacını başlangıçta sıfıra ayarla
 table_count = 0
+
+# Her slayttaki şekilleri kontrol et
 for slide in presentation.slides:
     for shape in slide.shapes:
         if shape.has_table:
             
             table = shape.table
-            print(table)
-            if table and table.cell(0, 0).text:
-                print("{} tablonun 1. Hücre değeri: {}".format(table_count, table.cell(0, 0).text) )
-                table.cell(0,0).text = "ays was here"
-                print("{} tablonun 1. Hücre değeri: {}".format(table_count, table.cell(0, 0).text) )
-                table_count = table_count + 1
+            # Sadece 1. tabloyu kontrol et
+            if table_count == 1:
+                if table and table.cell(0, 0).text:
+                    print("{} tablonun 1. hücresi değeri: {}".format(table_count, table.cell(0, 0).text))
+                    table.cell(0, 0).text = "Yeni Değer"  # 1. hücreye yeni değeri atar
+                    print("{} tablonun 1. hücresi değeri: {}".format(table_count, table.cell(0, 0).text))
+                table_count += 1
+                break  # 1. tabloyu bulduktan sonra diğer tablolara bakmamak için döngüyü kırar
+            else: 
+                table_count +=1
 
-print("Toplam tablo sayıisi:", table_count)
+# Sunuyu kaydet
 presentation.save("06_ChangeOldTables.pptx")
